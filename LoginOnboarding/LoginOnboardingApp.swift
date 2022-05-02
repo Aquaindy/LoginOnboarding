1843347a03670611b9fd291492478bbe5ce303a5
Aquaindy <pete.nyandeh@gmail.com>
2022-05-02 09:28:12 -0400

    Initial Commit

---
+++ LoginOnboarding/LoginOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginOnboardingApp.swift
+//  LoginOnboarding
+//
+//  Created by Pete Nyandeh Chie on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
